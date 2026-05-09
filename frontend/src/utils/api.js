ef6c708df0db4602f1d354eb6dd6cf98ca8b46bb
Deepti <deeptiverma048@gmail.com>
2026-05-10 00:32:25 +0530

    update api url to production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://team-task-manager-9vjx.onrender.com/api',
 });
 
 // attach token to every request automatically
